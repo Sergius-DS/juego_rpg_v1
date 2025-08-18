3117e7d45f88618775e1eb2ad0820f795dad0cff
Sergius-DS <sergioskhy@gmail.com>
2025-08-18 08:56:29 -0400

    Update objeto.py

--- objeto.py
+++ objeto.py
@@ -1,5 +1,5 @@
 class Objeto:
-    def __init__(self, nombre, descripcion):
+    def __init__(self, nombre, descripcion, emoji=""):
         self.nombre = nombre
         self.descripcion = descripcion
-        
+        self.emoji = emoji
